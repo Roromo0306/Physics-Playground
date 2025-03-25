9068a71b61125f4d8066c9987e6f40eff795bcdf
LuciaVargas0525 <lucia.storage25@gmail.com>
2025-03-25 11:04:37 +0100

    FPS y coger objeto

--- Physics Playground/Assets/Scripts/PlayerMovement.cs
+++ Physics Playground/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
+    Vector3 velocity;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +30,16 @@ public class PlayerMovement : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0 )
+        {
+            velocity.y = -2f;
+        }
     }
 }
